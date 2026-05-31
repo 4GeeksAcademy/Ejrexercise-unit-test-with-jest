b0e4d0ba2f86581abd92f6aa130ad015a693288f
Ejrodriguez3636 <ejrodriguez3636@gmail.com>
2026-05-31 20:20:43 +0000

    jest

---
+++ app.js
@@ -0,0 +1,18 @@
+// This is my function that sums two numbers
+const sum = (a,b) => {
+    return a + b
+}
+
+// Just a console log for ourselves
+console.log(sum(7,3))
+
+// Declaramos una función con el nombre exacto "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+
+
+module.exports = { sum, fromEuroToDollar }
