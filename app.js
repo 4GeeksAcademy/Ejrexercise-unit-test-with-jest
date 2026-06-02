ae2ac2b8e7ec5fdbee89ea6e8fd90571ab2787e7
Ejrodriguez3636 <ejrodriguez3636@gmail.com>
2026-06-02 11:12:09 +0000

    terminado

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
 
-let oneEuroIs = {
-    "JPY": 156.5, // Yen japonés
-    "USD": 1.07,  // Dólar estadounidense
-    "GBP": 0.87   // Libra esterlina
-};
-
-//  Euros a Dólares
-function fromEuroToDollar(amountInEuro) {
-    return amountInEuro * oneEuroIs["USD"];
+const sum = (a, b) => {
+    return a + b;
 }
 
-// Dólares a Yenes
-function fromDollarToYen(amountInDollar) {
+
+let oneEuroIs = {
+    "JPY": 156.5,
+    "USD": 1.07,
+    "GBP": 0.87
+};
+
+
+const fromEuroToDollar = function(valueInEuro) {
+    let valueInDollar = valueInEuro * 1.07;
+    return valueInDollar;
+}
+
+
+const fromDollarToYen = function(amountInDollar) {
     let amountInEuro = amountInDollar / oneEuroIs["USD"];
     return amountInEuro * oneEuroIs["JPY"];
 }
 
-//  Yenes a Libras
-function fromYenToPound(amountInYen) {
+const fromYenToPound = function(amountInYen) {
     let amountInEuro = amountInYen / oneEuroIs["JPY"];
     return amountInEuro * oneEuroIs["GBP"];
 }
 
 
-module.exports = { 
-    fromEuroToDollar, 
-    fromDollarToYen, 
-    fromYenToPound 
-};
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
--- test.js
+++ test.js
@@ -1,13 +1,26 @@
-// Importar la función sum del archivo app.js
-const { sum } = require('./app.js');
 
-// Comienza tu primera prueba
+const { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound } = require('./app.js');
+
+// 2. Prueba original de la suma
 test('adds 14 + 9 to equal 23', () => {
-    // Dentro de la prueba llamamos a nuestra función sum con 2 números
     let total = sum(14, 9);
-
-    // Esperamos que la suma de esos 2 números sea 23
     expect(total).toBe(23);
 });
 
-test.js
+
+test("one euro should be 1.07 dollars", function() {
+    const dollars = fromEuroToDollar(3.5);
+    expect(dollars).toBe(3.745); 
+});
+
+
+test("1.07 dollars should be 156.5 yen", function() {
+    const yen = fromDollarToYen(1.07);
+    expect(yen).toBe(156.5); 
+});
+
+
+test("156.5 yen should be 0.87 pounds", function() {
+    const pounds = fromYenToPound(156.5);
+    expect(pounds).toBe(0.87); 
+});
