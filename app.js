17f4efbf93d9dfee9837b38240004b57c1f7e46b
Ejrodriguez3636 <ejrodriguez3636@gmail.com>
2026-06-02 10:53:01 +0000

    casi2

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-// This is my function that sums two numbers
-const sum = (a,b) => {
-    return a + b
+
+let oneEuroIs = {
+    "JPY": 156.5, // Yen japonés
+    "USD": 1.07,  // Dólar estadounidense
+    "GBP": 0.87   // Libra esterlina
+};
+
+//  Euros a Dólares
+function fromEuroToDollar(amountInEuro) {
+    return amountInEuro * oneEuroIs["USD"];
 }
 
-// Just a console log for ourselves
-console.log(sum(7,3))
+// Dólares a Yenes
+function fromDollarToYen(amountInDollar) {
+    let amountInEuro = amountInDollar / oneEuroIs["USD"];
+    return amountInEuro * oneEuroIs["JPY"];
+}
 
-// Declaramos una función con el nombre exacto "fromEuroToDollar"
-const fromEuroToDollar = function(valueInEuro) {
-    // Convertimos el valor a dólares
-    let valueInDollar = valueInEuro * 1.07;
-    // Retornamos el valor en dólares
-    return valueInDollar;
+//  Yenes a Libras
+function fromYenToPound(amountInYen) {
+    let amountInEuro = amountInYen / oneEuroIs["JPY"];
+    return amountInEuro * oneEuroIs["GBP"];
 }
 
 
-module.exports = { sum, fromEuroToDollar }
+module.exports = { 
+    fromEuroToDollar, 
+    fromDollarToYen, 
+    fromYenToPound 
+};
